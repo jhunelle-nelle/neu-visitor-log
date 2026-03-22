859fdcd4ba73a0411e1e3b8c12e4af298728fc83
nelle <jhunelleremo71@gmail.com>
2026-03-22 15:37:43 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
 import { AuthProvider } from "@/hooks/useAuth";
 import Index from "@/pages/Index";
 import LoginPage from "@/pages/LoginPage";
+import VisitorForm from "@/pages/VisitorForm";
 import AdminDashboard from "@/pages/AdminDashboard";
 import NotFound from "@/pages/NotFound";
 
@@ -9,9 +11,11 @@ const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Toaster richColors position="top-center" />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/visitor" element={<VisitorForm />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
